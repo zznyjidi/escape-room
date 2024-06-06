2198db0c07972343b496e5d29aa4637d22c60d04
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-06-06 11:56:32 -0400

    Add theme

--- module/App.py
+++ module/App.py
@@ -1,7 +1,7 @@
 import ttkbootstrap as ttk
 
 class App(ttk.Window):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, theme: str = "darkly", debug: bool = False):
+        super().__init__(themename=theme)
     def startApp(self):
         self.mainloop()
