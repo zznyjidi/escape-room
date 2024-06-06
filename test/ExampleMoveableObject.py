b92b783e19dd4201a639ff86274d7fbea8d90aec
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-06-06 10:02:14 -0400

    Update DebugLog

--- test/ExampleMoveableObject.py
+++ test/ExampleMoveableObject.py
@@ -1,7 +1,9 @@
+import time
+
 class MoveableObject:
     def __init__(self):
         pass
     def isMoveable(self):
         return True
     def move(self, moveInfo):
-        print(moveInfo)
+        print(f"[{time.asctime()}][ExampleMoveableObject] {moveInfo}")
