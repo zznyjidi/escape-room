2bfdcf667368495389b31babb1769e8003e4b07f
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-06-07 10:34:04 -0400

    Add Useful Args

--- module/App.py
+++ module/App.py
@@ -1,8 +1,12 @@
 import ttkbootstrap as ttk
+from module.PlayerController import PlayerController
 
 class App(ttk.Window):
     def __init__(self, theme: str = "darkly", debug: bool = False):
         self.__debug = debug
         super().__init__(themename=theme)
+        self.__playerController: PlayerController = PlayerController(self, debug=debug)
+
+
     def startApp(self):
         self.mainloop()
--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 from module.App import App
+import sys
 
-appMain = App()
+try:
+    debug: bool = True if "--debug" in sys.argv else False
+    theme: str = (sys.argv[sys.argv.index("--theme")+1]) if ("--theme" in sys.argv) else "darkly"
+except IndexError as e:
+    print(f"Invalid Args: {e}")
+    exit()
+
+appMain = App(theme=theme, debug=debug)
+appMain.startApp()
