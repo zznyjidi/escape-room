3363c4c5011fdcaf0dc90450fb67eee964bc8d75
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-06-07 09:43:07 -0400

    Add Debug Mode

--- module/App.py
+++ module/App.py
@@ -2,6 +2,7 @@ import ttkbootstrap as ttk
 
 class App(ttk.Window):
     def __init__(self, theme: str = "darkly", debug: bool = False):
+        self.__debug = debug
         super().__init__(themename=theme)
     def startApp(self):
         self.mainloop()
