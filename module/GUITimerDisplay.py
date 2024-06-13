d6eba50b3f2f99630bbfb5518b4c1f505456c7b1
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-06-13 02:23:16 -0400

    Add DocStr and pack&place Functions

--- module/GUITimerDisplay.py
+++ module/GUITimerDisplay.py
@@ -2,10 +2,46 @@ from module.GlobalTimer import timerDisplay
 import ttkbootstrap as ttk
 
 class GUITimerDisplay(timerDisplay):
-    def __init__(self, master):
+    def __init__(self, *args, **kwargs):
+        """
+        #### GUI Timer Display Label. 
+        Display time left when attached to a timer. 
+        Args will be pass into Label Creation. 
+        """
         super().__init__()
-        self.TimerLabel = ttk.Label(master)
+        self.TimerLabel: ttk.Label = ttk.Label(*args, **kwargs)
 
     def update(self, timeSec: int):
+        """
+        #### Update the Timer Display Label. 
+
+        Args:
+            timeSec (int): Time Left in Second. 
+        """
         super().update(timeSec)
         self.TimerLabel.config(text="{}:{:02d}:{:02d}".format(self.hour, self.mins, self.secs))
+
+    def pack(self, *args, **kwargs):
+        """
+        #### Pack the Label. 
+        Args will be pass into Label.pack. 
+        """
+        self.TimerLabel.pack(*args, **kwargs)
+
+    def pack_forget(self):
+        """
+        #### Pack Forget the Label. 
+        """
+        self.TimerLabel.pack_forget()
+
+    def place(self, *args, **kwargs):
+        """
+        #### Place the Label. 
+        """
+        self.TimerLabel.place(*args, **kwargs)
+
+    def place_forget(self):
+        """
+        #### Place Forget the Label. 
+        """
+        self.TimerLabel.place_forget()
