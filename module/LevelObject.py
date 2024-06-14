702bad288b1b4ab47f0ffae5cd4f61e098a6ba08
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-06-14 09:22:13 -0400

    Allow Interactive

--- module/LevelObject.py
+++ module/LevelObject.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageTk
 from ttkbootstrap import Canvas, NW
-from typing import Tuple, List, Any
+from typing import Tuple, List, Any, Callable
 
 class LevelObject:
     objectType: str = "BASE"
@@ -11,14 +11,21 @@ class LevelObject:
         self.targetCanvas = canvas
         self.targetCoordinate = (coordinate[1], coordinate[0])
 
+    def interactive(self) -> Callable[[None], None] | None:
+        return self.interactiveFunction
+
     def getObjectType(self) -> str:
         return self.objectType
+
+    def isInteractive(self) -> bool:
+        return self.interactiveFunction != None
 
 class img(LevelObject):
     __counter: int = -1
     __PhotoImg: List[ImageTk.PhotoImage] = []
-    def __init__(self, imgPath: str, size: Tuple[int, int]):
+    def __init__(self, imgPath: str, size: Tuple[int, int], interactive: Callable[[None], None] | None = None):
         self.objectType = "IMAGE"
+        self.interactive = interactive
         self.object: Image = Image.open(imgPath)
         self.object.thumbnail(size)
 
