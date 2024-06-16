effcc1744442b94df1ad247cf405c5a3b4d8e6b2
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-06-16 00:31:07 -0400

    Fix Frame Settings

--- config/drawing.py
+++ config/drawing.py
@@ -3,17 +3,17 @@ from typing import Dict, Tuple
 gridBlockSize: int = 64
 
 tileConfig: Dict[str, Tuple[int, int]] = {
-    "STAND_DOWN":  (0, 4),
-    "STAND_UP":    (0, 2),
-    "STAND_LEFT":  (0, 3),
-    "STAND_RIGHT": (0, 1),
+    "STAND_DOWN":  (0, 3),
+    "STAND_UP":    (0, 1),
+    "STAND_LEFT":  (0, 2),
+    "STAND_RIGHT": (0, 0),
 
-    "WALK_DOWN_START":  (9, 36),
-    "WALK_DOWN_STOP":   (9, 47),
-    "WALK_UP_START":    (9, 12),
-    "WALK_UP_STOP":     (9, 23),
-    "WALK_LEFT_START":  (9, 24),
-    "WALK_LEFT_STOP":   (9, 35),
-    "WALK_RIGHT_START": (9, 0),
-    "WALK_RIGHT_STOP":  (9, 11),
+    "WALK_DOWN_START":  (16, 12),
+    "WALK_DOWN_STOP":   (16, 15),
+    "WALK_UP_START":    (16, 4),
+    "WALK_UP_STOP":     (16, 7),
+    "WALK_LEFT_START":  (16, 8),
+    "WALK_LEFT_STOP":   (16, 11),
+    "WALK_RIGHT_START": (16, 0),
+    "WALK_RIGHT_STOP":  (16, 3),
 }
