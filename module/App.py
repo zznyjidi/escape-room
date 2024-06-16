a5032869f0a6e3e219d8e3c400d130db9a27f823
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-06-16 00:34:38 -0400

    Update

--- module/App.py
+++ module/App.py
@@ -1,12 +1,13 @@
 import ttkbootstrap as ttk
 from module.PlayerController import PlayerController
+from module.LevelBuilder import LevelBuilder
 
 class App(ttk.Window):
     def __init__(self, theme: str = "darkly", debug: bool = False):
         self.__debug = debug
         super().__init__(themename=theme)
-        self.__playerController: PlayerController = PlayerController(self, debug=debug)
-
+        self.playerController: PlayerController = PlayerController(self, debug=debug)
+        self.levelFrame: LevelBuilder = LevelBuilder(self, debug=debug)
 
     def startApp(self):
         self.mainloop()
