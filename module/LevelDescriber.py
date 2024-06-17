dd9b0d9ceb19a5e4343af28f9c9a723613d8bac3
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-06-17 01:56:39 -0400

    Add DocStr

--- module/LevelDescriber.py
+++ module/LevelDescriber.py
@@ -13,10 +13,24 @@ class LevelDescriber:
     WIDTH: int
 
     def __init__(self, *, NextLevelPos: Tuple[int, int], RequireUnlock: bool=True):
+        """
+        #### Level Configs. 
+
+        Args:
+            NextLevelPos (Tuple[int, int]): Next Level Block. 
+            RequireUnlock (bool, optional): Require Unlock to Go to Next Level. Defaults to True.
+        """
         self.NEXT_LEVEL = NextLevelPos
         self.UNLOCKED = not RequireUnlock
 
     def setDescriber(self, Describer: List[List[LevelObject | PlaceHolder | None]], wrap: bool=True):
+        """
+        #### Set Level Describer for the Level. 
+
+        Args:
+            Describer (List[List[LevelObject  |  PlaceHolder  |  None]]): Level Describer. 
+            wrap (bool, optional): Wrap Level with Hitbox. Defaults to True.
+        """
         self.DESCRIBER = []
         for row in Describer:
             self.DESCRIBER.append(row[:])
@@ -31,10 +45,22 @@ class LevelDescriber:
         self.GRID.importGrid(self.DESCRIBER)
 
     def setLockedFunction(self, func: Callable[[None], None]):
+        """
+        #### Set Function to Run when Trying to Go to Next Level Without Unlock. 
+
+        Args:
+            func (Callable[[None], None]): Function to Run. 
+        """
         self.LOCKED = func
 
     def lock(self):
+        """
+        #### Lock the Level. 
+        """
         self.UNLOCKED = False
 
     def unlock(self):
+        """
+        #### Unlock the Level. 
+        """
         self.UNLOCKED = True
