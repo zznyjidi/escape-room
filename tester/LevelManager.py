422c9a19d6b8b07e7b043cf7d67bfd4bd1a0c7f7
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-06-17 09:07:58 -0400

    BugFix: Screenshot

--- tester/LevelManager.py
+++ tester/LevelManager.py
@@ -11,6 +11,6 @@ game.addLevel(levels.testLevel.LEVEL, levels.testLevel.LEVEL_LOCKED)
 game.addPlayerTiles(TileLoader("assets/player_tile_32.png", tileSize=(32, 48), blankHeight=16, emptyFirstRow=True))
 game.initGame(window)
 
-#window.after(1000, lambda: level.toImage().show())
+#window.after(1000, lambda: game.levelBuilder.toImage().show())
 
 window.mainloop()
